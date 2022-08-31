e4a497e960be6423fabbde18984cfee616e1b91c
lexuanhai9x <110213939+lexuanhai9x@users.noreply.github.com>
2022-08-31 13:45:50 +0700

    vidu 16

---
+++ vidu16/vd1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main(){
+    printf("\nI am in main");
+    italy();
+    brazil();
+    argentina();
+
+}
+italy(){
+    printf("\nI am in italy");
+}
+brazil(){
+    printf("\nI am in brazil");
+}
+argentina(){
+    printf("\nI am in argentina");
+}
